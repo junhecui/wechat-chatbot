dabee39839b5fa6ee1148805f4c6138c7ef2700e
junhe-c <cjunhe05@gmail.com>
2024-05-10 16:10:31 +0800

    added comments and edited bot.ts

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 * Automatically respond to messages containing keywords, mentions user that sends message.
 * When message does not contain keywords or user is unsatisfied, messages administrator with question.
   * Administrator response will be relayed to the user.
+
+## Instructions
+
+1. Clone repository
+2. npm install
+3. Scan QR code with account you want it the bot to function on
+  
--- bot.ts
+++ bot.ts
@@ -1,71 +1,74 @@
 import 'dotenv/config.js'
 
 import {
-  Contact,
-  Message,
-  ScanStatus,
-  WechatyBuilder,
-  log,
-}                  from 'wechaty'
+    Contact,
+    Message,
+    ScanStatus,
+    WechatyBuilder,
+    log,
+} from 'wechaty'
 
 import qrcodeTerminal from 'qrcode-terminal'
 
-function onScan (qrcode: string, status: ScanStatus) {
-  if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
-    const qrcodeImageUrl = [
-      'https://wechaty.js.org/qrcode/',
-      encodeURIComponent(qrcode),
-    ].join('')
-    log.info('Bot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
+function onScan(qrcode: string, status: ScanStatus) {
+    if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
+        const qrcodeImageUrl = [
+            'https://wechaty.js.org/qrcode/',
+            encodeURIComponent(qrcode),
+        ].join('')
+        log.info('Bot', 'onScan: %s(%s) - %s', ScanStatus[status], status, qrcodeImageUrl)
 
-    qrcodeTerminal.generate(qrcode, { small: true }) 
+        qrcodeTerminal.generate(qrcode, { small: true })
 
-  } else {
-    log.info('Bot', 'onScan: %s(%s)', ScanStatus[status], status)
-  }
+    } else {
+        log.info('Bot', 'onScan: %s(%s)', ScanStatus[status], status)
+    }
 }
 
-function onLogin (user: Contact) {
-  log.info('Bot', '%s login', user)
+function onLogin(user: Contact) {
+    log.info('Bot', '%s login', user)
 }
 
-function onLogout (user: Contact) {
-  log.info('Bot', '%s logout', user)
+function onLogout(user: Contact) {
+    log.info('Bot', '%s logout', user)
 }
 
-async function onMessage (msg: Message) {
-    if (msg.text().startsWith('@'.concat('temp '))) {
-        let message = msg.text().substring(6)
-        log.info('Bot', message.toString())        
-        
+async function onMessage(msg: Message) {
+    if (msg.text().startsWith('!'.concat('bot '))) { // acts as a command '!bot help'
+        let message = msg.text().substring(5)
+        log.info('Bot', message.toString())
+
         const sender = msg.talker()
         log.info('Contact Id:', sender?.id, 'Name:', sender?.name())
 
         if (message.toLowerCase().includes("hi") || message.toLowerCase().includes("hello")) {
             await msg.say('Hello ' + sender?.name() + "!")
         }
+        if (message.toLowerCase().includes("help")) {
+            await msg.say('What do you need help with?') // list out common options
+        }
         else {
             msg.say('抱歉, ' + sender?.name())
-            const forwardRecipient = await bot.Contact.find({id: '16043551810@c.us'})
-            
-            if (forwardRecipient) {
-                msg.forward(forwardRecipient)
-                log.info('forwarded', msg, 'to', forwardRecipient?.name())
-            }
+            // const forwardRecipient = await bot.Contact.find({id: '16043551810@c.us'}) // replace with admin id or name
+
+            // if (forwardRecipient) {
+            //     msg.forward(forwardRecipient)
+            //     log.info('forwarded', msg, 'to', forwardRecipient?.name())
+            // }
         }
     }
 }
 
 const bot = WechatyBuilder.build({
-  name: 'bot',
-  puppet: 'wechaty-puppet-whatsapp',
+    name: 'bot',
+    puppet: 'wechaty-puppet-whatsapp',
 })
 
-bot.on('scan',    onScan)
-bot.on('login',   onLogin)
-bot.on('logout',  onLogout)
+bot.on('scan', onScan)
+bot.on('login', onLogin)
+bot.on('logout', onLogout)
 bot.on('message', onMessage)
 
 bot.start()
-  .then(() => log.info('Bot', 'Bot Started.'))
-  .catch(e => log.error('Bot', e))
+    .then(() => log.info('Bot', 'Bot Started.'))
+    .catch(e => log.error('Bot', e))
